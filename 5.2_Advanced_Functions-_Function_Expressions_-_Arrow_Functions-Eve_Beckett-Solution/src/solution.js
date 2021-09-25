eeec42dcd05b53a716d905b0f8cd253dcc1778bd
Eve Beckett <evebeckettwriter@gmail.com>
2021-09-25 11:30:47 -0400

    Arrow Functions

---
+++ 5.2_Advanced_Functions-_Function_Expressions_-_Arrow_Functions-Eve_Beckett-Solution/src/solution.js
@@ -0,0 +1,33 @@
+/*
+  Convert the following volume calculating functions into arrow functions. Do not change the names of the functions.
+*/
+
+const calculateCubeVolume = length => length ** 3;
+// function calculateCubeVolume(length) {
+//   return length ** 3;
+// }
+
+const calculateSphereVolume = radius => {
+  return (4 / 3) * Math.PI * radius ** 3;
+};
+// function calculateSphereVolume(radius) {
+//   return (4 / 3) * Math.PI * radius ** 3;
+// }
+
+const calculateConeVolume = (radius, height) => {
+  return (1 / 3) * Math.PI * radius ** 2 * height;
+};
+
+const calculateCylinderVolume = (radius, height) => {
+  return Math.PI * radius ** 2 * height;
+};
+
+/////////////////////////////////////
+// Do not edit anything below!
+/////////////////////////////////////
+module.exports = {
+  calculateCubeVolume,
+  calculateSphereVolume,
+  calculateConeVolume,
+  calculateCylinderVolume,
+};
