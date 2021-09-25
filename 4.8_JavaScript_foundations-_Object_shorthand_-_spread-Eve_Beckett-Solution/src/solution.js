6e7c36eb0304965c3bd43c9891a541fb2ab2a1df
Eve Beckett <evebeckettwriter@gmail.com>
2021-09-25 11:07:27 -0400

    Object Shorthand

---
+++ 4.8_JavaScript_foundations-_Object_shorthand_-_spread-Eve_Beckett-Solution/src/solution.js
@@ -0,0 +1,31 @@
+/*
+  Modify each function below to continue working with the suggested syntax.
+  
+  When a function's parameters reference `product`, it is referring to an object. Each object has the following shape:
+   {
+     name: "Washed Black Denim Overalls",
+     priceInCents: 12000,
+     availableSizes: [ 28, 30, 32, 36 ]
+   }
+*/
+
+// `salesTax` is a decimal number, like 0.065
+function createSalesProduct(product, salesTax) {
+  let {name, availableSizes, priceInCents} = product;
+  return {
+    name,
+    availableSizes,
+    salesTax,
+    priceInCents: priceInCents * (1 + salesTax),
+  };
+}
+
+function joinSizes(productA, productB) {
+  
+  const result = [...productA.availableSizes, ...productB.availableSizes];
+  return result;
+} 
+module.exports = {
+  createSalesProduct,
+  joinSizes,
+};
